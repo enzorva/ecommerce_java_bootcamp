aec1266bd48a9bc068a176d1f36f47743141edda
Ubiratan Tavares <ust1973@gmail.com>
2025-09-12 21:07:45 -0300

    criação da classe Repository

---
+++ src/repository/Repository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import java.util.List;
+
+public interface Repository<T, E> {
+
+    T save(T obj);
+
+    T update(T obj);
+
+    List<T> findAll();
+
+    T findById(E id);
+}
