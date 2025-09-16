93ccb9dd9c51eb13922ee4848954ca0f5e23c714
Ubiratan Tavares <ust1973@gmail.com>
2025-09-16 10:21:47 -0300

    atualizando o main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
-
-
+import controller.ClientController;
+import controller.ClientControllerImpl;
+import model.ClientType;
+import repository.ClientRepository;
+import validation.ClientValidator;
+import validation.GenericClientValidator;
 
 public class Main {
     public static void main(String[] args) {
+
+        ClientRepository repository = new ClientRepository();
+        ClientValidator validator = new GenericClientValidator();
+        
+        ClientController controller = new ClientControllerImpl(repository, validator);
+
+        controller.createClient(ClientType.PF);
+        controller.listAllClients();
+        controller.updateClient();
+        controller.searchByDocument();
+        
+        
 
     }
 }
